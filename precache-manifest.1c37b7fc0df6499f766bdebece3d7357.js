1dcb6b54d8195e5956a816c2190c3d3a3492f56a
Andrey <a.harkovets@gmail.com>
2019-12-02 11:20:53 +0200

    Build #1

---
+++ precache-manifest.1c37b7fc0df6499f766bdebece3d7357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbede827bfb361cb7a65f7b52cad51ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb9d895a654b6507226",
+    "url": "/static/css/main.f6436782.chunk.css"
+  },
+  {
+    "revision": "b497a6c08783b60eba2d",
+    "url": "/static/js/2.08967835.chunk.js"
+  },
+  {
+    "revision": "ceb9d895a654b6507226",
+    "url": "/static/js/main.b62e1824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ac9a3eb6ad3fdae81ede0177655298f",
+    "url": "/static/media/avatar.9ac9a3eb.jpg"
+  }
+]);
